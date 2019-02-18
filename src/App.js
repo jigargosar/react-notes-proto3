11b4cac52f0a2d4157a082b40b6693f9be791fa2
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-18 17:33:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@
 import React, { useState } from 'react'
 import { getCached } from './dom-helpers'
 import { useCacheEffect } from './hooks'
+import validate from 'aproba'
+import * as R from 'ramda'
+
+function applyIfFunction(fnOrValue, ...args) {
+  validate('*A', [fnOrValue, args])
+  if (R.is(Function)(fnOrValue)) {
+    return R.apply(fnOrValue, args)
+  } else {
+    return fnOrValue
+  }
+}
+
+function useLocalStorage(key, preProcess) {
+  const [state, setState] = useState(() => {
+    return preProcess(getCached(key))
+  })
+  useCacheEffect(key, state)
+  return [state, setState]
+}
 
 function App() {
-  const initialState = { fahrenheit: 70, other: {} }
-
-  const [state, setState] = useState(() => getCached('app-state') || {})
-  useCacheEffect('app-state', state)
+  const initialState = {}
+  const [state, setState] = useLocalStorage(
+    'app-state',
+    R.pipe(
+      R.defaultTo({}),
+      R.mergeDeepRight({ ct: 0 }),
+    ),
+  )
 
   return (
-    <div>
-      <div>Global app state: {JSON.stringify(state)}</div>
-    </div>
+    state && (
+      <div>
+        <div>Global app state: {JSON.stringify(state)}</div>
+        <div>ct:{state.ct + 1}</div>
+      </div>
+    )
   )
 }
 
