796df6de0857f9053f144e228e13110df15d6d63
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-18 17:46:32 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import React from 'react'
 import { useLocalStorage } from './hooks'
 import { ErrorBoundary } from './ErrorBoundary'
 import { mergeDefaults } from './ramda-helpers'
+import * as R from 'ramda'
+
+const getVisibleNotes = pipe([
+  R.prop('notesById'),
+  R.values,
+  R.sortWith([R.descend(R.propOr(0, 'modifiedAt'))]),
+])
 
 function App() {
   const [state, setState] = useLocalStorage(
     'app-state',
-    mergeDefaults({ ct: 0 }),
+    mergeDefaults({ ct: 0, notesById: {} }),
   )
+
+  const visibleNotes = getVisibleNotes(state)
 
   return (
     <ErrorBoundary>
       <div>Global app state: {JSON.stringify(state)}</div>
       <div>ct:{state.ct + 1}</div>
+      {visibleNotes.map(note => (
+        <div className="pa3 bb b--moon-gray">{note.content}</div>
+      ))}
     </ErrorBoundary>
   )
 }
