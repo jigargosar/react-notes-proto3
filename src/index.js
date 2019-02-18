8f75a85dcc89e884f465a6d0ef101083de55eb3d
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-18 17:57:06 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import 'tachyons'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import * as ReactDOM from 'react-dom'
 
 function render() {
   ReactDOM.render(<App />, document.getElementById('root'))
