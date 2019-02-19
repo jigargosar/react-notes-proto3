3d6548323b27c2c2ed8b52c62615c049635f19ac
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-19 16:45:08 +0530

    pouch

--- src/store-model.js
+++ src/store-model.js
@@ -24,6 +24,40 @@ const getVisibleNotes = pipe([
   R.sortWith([R.descend(R.propOr(0, 'modifiedAt'))]),
 ])
 
+const notesModel = {
+  byId: {},
+  selectedId: null,
+  visibleNotes: select(getVisibleNotes),
+  add: (state, note) =>
+    pipe([R.assocPath(['byId', note._id])(note)])(state),
+  addNew: thunk(async actions => {
+    const note = createNewNote()
+    await db.put(note)
+  }),
+  remove: thunk(async (actions, note) => {
+    await db.put({ ...note, _deleted: true })
+  }),
+  replaceAll(state, docs) {
+    state.byId = pouchDocsToIdLookup(docs)
+  },
+  handleChange: (state, change) => {
+    const note = change.doc
+    const mergeNote = R.assocPath(['byId', note._id])(note)
+    const omitNote = R.dissocPath(['byId', note._id])
+
+    const update = change.deleted ? omitNote : mergeNote
+    return update(state)
+  },
+  initFromPouch: thunk(async (actions, payload) => {
+    const { rows } = await db.allDocs({ include_docs: true })
+    const docs = rows.map(R.prop('doc'))
+    actions.replaceAll(docs)
+    return db
+      .changes({ include_docs: true, live: true, from: 'now' })
+      .on('change', actions.handleChange)
+      .on('error', console.error)
+  }),
+}
 export const storeModel = {
   debug: {
     inspectorVisible: true,
@@ -38,40 +72,7 @@ export const storeModel = {
       // (you can also return a new immutable instance if you prefer)
     },
   },
-  notes: {
-    byId: {},
-    selectedId: null,
-    visibleNotes: select(getVisibleNotes),
-    add: (state, note) =>
-      pipe([R.assocPath(['byId', note._id])(note)])(state),
-    addNew: thunk(async actions => {
-      const note = createNewNote()
-      await db.put(note)
-    }),
-    remove: thunk(async (actions, note) => {
-      await db.put({ ...note, _deleted: true })
-    }),
-    replaceAll(state, docs) {
-      state.byId = pouchDocsToIdLookup(docs)
-    },
-    handleChange: (state, change) => {
-      const note = change.doc
-      const mergeNote = R.assocPath(['byId', note._id])(note)
-      const omitNote = R.dissocPath(['byId', note._id])
-
-      const update = change.deleted ? omitNote : mergeNote
-      return update(state)
-    },
-    initFromPouch: thunk(async (actions, payload) => {
-      const { rows } = await db.allDocs({ include_docs: true })
-      const docs = rows.map(R.prop('doc'))
-      actions.replaceAll(docs)
-      return db
-        .changes({ include_docs: true, live: true, from: 'now' })
-        .on('change', actions.handleChange)
-        .on('error', console.error)
-    }),
-  },
+  notes: notesModel,
 }
 
 function pouchDocsToIdLookup(docs) {
