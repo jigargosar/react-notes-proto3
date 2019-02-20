eff7e0cbb770f496c578265b717867dfd64335fa
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-20 16:15:14 +0530

    edit note

--- src/EditNoteDialog.js
+++ src/EditNoteDialog.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Dialog from '@material-ui/core/Dialog'
+import { withStyles } from '@material-ui/core/styles'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import TextField from '@material-ui/core/TextField'
@@ -7,40 +8,45 @@ import DialogActions from '@material-ui/core/DialogActions'
 import Button from '@material-ui/core/Button'
 import withMobileDialog from '@material-ui/core/withMobileDialog'
 import { useNoteActions } from './store-model'
+import { pipe } from './ramda-helpers'
 
-export const EditDialog = withMobileDialog({ breakpoint: 'xs' })(
-  function EditDialog({ note, fullScreen }) {
-    const { closeEditDialog, saveNoteContent } = useNoteActions()
+export const EditDialog = pipe([
+  withMobileDialog({ breakpoint: 'xs' }),
+  withStyles({ dialogActions: { justifyContent: 'flex-start' } }),
+])(function EditDialog({ note, fullScreen, classes }) {
+  const { closeEditDialog, saveNoteContent } = useNoteActions()
 
-    const [content, setContent] = useState(() => note.content)
-    const onClose = () => closeEditDialog()
-    const onSave = () => {
-      saveNoteContent({ content, note })
-    }
-    return (
-      <Dialog onClose={onClose} open={true} fullScreen={fullScreen}>
-        <DialogTitle>Edit Note</DialogTitle>
-        <DialogContent style={{ minWidth: '400px' }}>
-          <TextField
-            autoFocus
-            multiline
-            value={content}
-            onChange={e => setContent(e.target.value)}
-            // className={classes.textField}
-            margin="normal"
-            fullWidth
-            variant="outlined"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose} color="primary">
-            Discard
-          </Button>
-          <Button onClick={onSave} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-    )
-  },
-)
+  const [content, setContent] = useState(() => note.content)
+  const onClose = () => closeEditDialog()
+  const onSave = () => {
+    saveNoteContent({ content, note })
+  }
+  return (
+    <Dialog onClose={onClose} open={true} fullScreen={fullScreen}>
+      <DialogTitle>Edit Note</DialogTitle>
+      <DialogContent style={{ minWidth: '400px' }}>
+        <TextField
+          autoFocus
+          multiline
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          // className={classes.textField}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+        />
+      </DialogContent>
+      <DialogActions
+        className="flex flex-row-reverse justify-start"
+        classes={{ root: classes.dialogActions }}
+      >
+        <Button onClick={onSave} color="primary">
+          Save
+        </Button>
+        <Button onClick={onClose} color="primary">
+          Discard
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+})
