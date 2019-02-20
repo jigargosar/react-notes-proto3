92219948217556e703604a3170727c7f60afa39a
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-20 18:03:30 +0530

    settings

--- src/SettingsDialog.js
+++ src/SettingsDialog.js
@@ -34,7 +34,7 @@ export const SettingsDialog = enhance(function SettingsDialog({
     <Dialog
       onClose={onClose}
       open={isSettingsDialogOpen}
-      // fullScreen={fullScreen}
+      fullScreen={fullScreen}
     >
       <DialogTitle>Edit Note</DialogTitle>
       <DialogContent style={{ minWidth: '400px' }}>
