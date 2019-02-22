73b44300f8477982c532f029c5cb0761ec61a1cc
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-22 12:11:56 +0530

    theme

--- src/ui/theme/index.js
+++ src/ui/theme/index.js
@@ -1,11 +1,26 @@
+// import { createMuiTheme } from '@material-ui/core/styles'
+// import blue from '@material-ui/core/colors/blue'
+// import red from '@material-ui/core/colors/red'
+//
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: red,
+//   },
+//   typography: { useNextVariants: true },
+// })
+//
+// export default theme
+
+// src/ui/theme/index.js
+
 import { createMuiTheme } from '@material-ui/core/styles'
-import blue from '@material-ui/core/colors/blue'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: blue,
-  },
-  typography: { useNextVariants: true },
-})
+const palette = {
+  primary: { main: '#1E88E5' },
+  secondary: { main: '#BA68C8' },
+}
 
-export default theme
+const typography = { useNextVariants: true }
+const themeName = 'Curious Blue Amethyst Dog'
+
+export default createMuiTheme({ palette, typography, themeName })
