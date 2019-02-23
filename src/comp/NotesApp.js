a2b79271009bc7275175cf292010e0acda3cf215
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-23 16:03:56 +0530

    mst

--- src/comp/NotesApp.js
+++ src/comp/NotesApp.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useNotesActions } from './store-hooks'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
 // import MenuIcon from '@material-ui/icons/Menu'
@@ -7,8 +6,10 @@ import AddIcon from '@material-ui/icons/Add'
 import { SettingsDialog } from './SettingsDialog'
 import { NoteList } from './NoteList'
 import { TopAppBar } from './TopAppBar'
+import { rs } from '../store-mst'
+import { mc } from '../mob-act'
 
-export function NotesApp() {
+export const NotesApp = mc(function NotesApp() {
   return (
     <div className="pb5">
       <TopAppBar />
@@ -17,16 +18,14 @@ export function NotesApp() {
       <AddNoteFab />
     </div>
   )
-}
-
+})
 function AddNoteFab({ classes, ...otherProps }) {
-  const { addNewNote } = useNotesActions()
   return (
     <Fab
       className={`fixed bottom-1 right-1`}
       size="small"
       color="secondary"
-      onClick={() => addNewNote()}
+      onClick={rs.addNewNoteClicked}
       {...otherProps}
     >
       <AddIcon />
