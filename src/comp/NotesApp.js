a93ebb48156cce17ca59b20d125e93d2c9f3df84
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-23 16:04:42 +0530

    mst

--- src/comp/NotesApp.js
+++ src/comp/NotesApp.js
@@ -9,17 +9,7 @@ import { TopAppBar } from './TopAppBar'
 import { rs } from '../store-mst'
 import { mc } from '../mob-act'
 
-export const NotesApp = mc(function NotesApp() {
-  return (
-    <div className="pb5">
-      <TopAppBar />
-      <NoteList />
-      <SettingsDialog />
-      <AddNoteFab />
-    </div>
-  )
-})
-function AddNoteFab({ classes, ...otherProps }) {
+const AddNoteFab = mc(function AddNoteFab({ classes, ...otherProps }) {
   return (
     <Fab
       className={`fixed bottom-1 right-1`}
@@ -31,4 +21,15 @@ function AddNoteFab({ classes, ...otherProps }) {
       <AddIcon />
     </Fab>
   )
-}
+})
+
+export const NotesApp = mc(function NotesApp() {
+  return (
+    <div className="pb5">
+      <TopAppBar />
+      <NoteList />
+      <SettingsDialog />
+      <AddNoteFab />
+    </div>
+  )
+})
