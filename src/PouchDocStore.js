c8b776751b50a857ac72ffb8003ea698f7990022
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-23 17:53:17 +0530

    mst

--- src/PouchDocStore.js
+++ src/PouchDocStore.js
@@ -1,73 +1,9 @@
-import { addDisposer, flow as f, types as t } from 'mobx-state-tree'
+import { addDisposer, flow as f } from 'mobx-state-tree'
 import { values } from 'mobx'
 import validate from 'aproba'
 import { it } from 'param.macro'
 import { objFromList } from './ramda-helpers'
 import { getNodeName } from './mst-helpers'
-
-export function createPouchStore(modelType, cb) {
-  validate('OF', arguments)
-  const modelName =
-    modelType.name === 'AnonymousModel' ? 'AnonDoc' : modelType.name
-
-  const pouchModel = t
-    .model(`Pouch${modelName}Store`, {
-      byId: t.map(modelType),
-    })
-    .views(s => ({
-      get all() {
-        return values(s.byId)
-      },
-      docToModel(doc) {
-        validate('O', arguments)
-        return modelType.create(doc)
-      },
-    }))
-    .actions(s => ({
-      replaceAllDocs(docs) {
-        validate('A', arguments)
-        s.byId.replace(pouchDocsToIdLookup(docs))
-      },
-      putDoc(doc) {
-        validate('O', arguments)
-        s.byId.put(s.docToModel(doc))
-      },
-      remove(doc) {
-        validate('O', arguments)
-        s.byId.delete(doc._id)
-      },
-    }))
-    .actions(s => ({
-      _handlePouchChange(change) {
-        validate('OZZ', arguments)
-        console.debug(`change`, ...arguments)
-        const doc = change.doc
-
-        change.deleted ? s.remove(doc) : s.putDoc(doc)
-      },
-    }))
-    .actions(s => ({
-      initPouch: f(function*(db) {
-        const { rows } = yield db.allDocs({
-          include_docs: true,
-        })
-        const docs = rows.map(it.doc)
-        console.log(`[${getNodeName(s)}] initial docs`, docs)
-        s.replaceAllDocs(docs)
-        const changes = db
-          .changes({
-            include_docs: true,
-            live: true,
-            since: 'now',
-          })
-          .on('change', s._handlePouchChange)
-          .on('error', console.error)
-        addDisposer(s, () => changes.cancel())
-      }),
-    }))
-
-  return cb(pouchModel)
-}
 
 function pouchDocsToIdLookup(docs) {
   validate('A', arguments)
